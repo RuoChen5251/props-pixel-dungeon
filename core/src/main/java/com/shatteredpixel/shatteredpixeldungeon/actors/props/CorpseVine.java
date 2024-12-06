b639732f59dc410a0939bccaaa34a5f784b76d40
RuoChen <linx5251@qq.com>
2024-12-06 16:43:32 +0800

    一些调整

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/props/CorpseVine.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/props/CorpseVine.java
@@ -1,7 +1,13 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.props;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Regeneration;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.particles.LeafParticle;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.ui.PropIndicator;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.watabou.utils.Random;
@@ -25,8 +31,23 @@ public class CorpseVine extends Prop{
             if (Dungeon.hero.HP<Dungeon.hero.HT)
                 GLog.p(logText());
             Dungeon.hero.heal(hp);
+            plantGrass(mob.pos);
         }
 
+    }
+    private void plantGrass(int cell){
+        int t = Dungeon.level.map[cell];
+        if ((t == Terrain.EMPTY || t == Terrain.EMPTY_DECO || t == Terrain.EMBERS
+                || t == Terrain.GRASS || t == Terrain.FURROWED_GRASS)
+                && Dungeon.level.plants.get(cell) == null){
+            if (!Regeneration.regenOn()){
+                Level.set(cell, Terrain.FURROWED_GRASS);
+            } else {
+                Level.set(cell, Terrain.HIGH_GRASS);
+            }
+            GameScene.updateMap(cell);
+            CellEmitter.get( cell ).burst( LeafParticle.LEVEL_SPECIFIC, 1 );
+        }
     }
 
 }
